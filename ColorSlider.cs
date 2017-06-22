8841330069e182da38bc657b10ebdf3ea91b6cdf
fabricelacharme <fabrice.lacharme@gmail.com>
2017-06-22 16:04:43 +0200

    minors updates

--- ColorSlider.cs
+++ ColorSlider.cs
@@ -1,4 +1,41 @@
-﻿using System;
+﻿#region License
+
+/* Copyright (c) 2017 Fabrice Lacharme
+ * This code is inspired from Michal Brylka 
+ * https://www.codeproject.com/Articles/17395/Owner-drawn-trackbar-slider
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ */
+
+#endregion
+
+#region Contact
+
+/*
+ * Fabrice Lacharme
+ * Email: fabrice.lacharme@gmail.com
+ */
+
+#endregion
+
+
+using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -6,8 +43,10 @@ using System.Windows.Forms;
 
 namespace ColorSlider
 {
-
-    //https://www.codeproject.com/Articles/17395/Owner-drawn-trackbar-slider
+    // Original code from Michal Brylka on Code Project
+    // see https://www.codeproject.com/Articles/17395/Owner-drawn-trackbar-slider
+    // ColorSlider is a trackbar control written in C# as a replacement of the trackbar 
+    // proposed by Microsoft in Visual Studio
 
     /// <summary>
     /// Encapsulates control that visualy displays certain integer value and allows user to change it within desired range. It imitates <see cref="System.Windows.Forms.TrackBar"/> as far as mouse usage is concerned.
